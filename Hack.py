22db70b79303055263ad00cf4946f4e5994889f5
MASTERX <50529518+seifsherifssss@users.noreply.github.com>
2019-07-11 17:09:40 +0200

    Add files via upload

---
+++ Hack.py
@@ -0,0 +1,28 @@
+def ban () :
+    import os
+    os.system("pkg install cowsay axel curl php openssh openssl-tool perl ruby toilet figlet  unstable-repo x11-repo nmap -y && gem install lolcat && pkg update upgrade -y ")
+    import os
+    os.system("clear && cowthink -f eyes 'HAHAHAHA' | lolcat && toilet -f big 'GET READY ' | lolcat ")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/Xi4u7/A-Rat.git")
+    import os 
+    os.system("cd /$HOME && pkg install unstable-repo && pkg install metasploit -y ")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/king-hacking/SPY-KING.git ")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/Masoomarman/DarkSploit")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/neoneggplant/EggShell ")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/MrSqar-Ye/BadMod")
+    import os 
+    os.system("cd /$HOME && git clone https://github.com/seifsherifssss/BeeF && cd BeeF && bash beef.sh && bash FixBigdecimal.sh ")
+    import os 
+    os.system("clear && toilet -f big 'DONE HACKER YOU CAN HACK ANY PHONE NOW ' | lolcat ")
+    import os 
+    os.system("echo''")
+    import os
+    os.system("toilet -f big 'You Installed Beef.Metasploit.Eggshell.Wireshark.Aircrack.SPY-KING.A-Rat.Darksploit AND BadMod'")
+    import os 
+    os.system("toilet -f big 'ENJOY HACKING ....... !!!!'")
+ban()
